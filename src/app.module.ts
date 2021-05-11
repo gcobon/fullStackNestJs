85b5a42f95725e6fbcf24c95fa5eb77853e49300
Gudiel Cobon <gudielcobon@gmail.com>
2021-05-10 20:32:31 -0600

    deploy

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,22 @@
+import { validationSchema } from './config/validation';
+import { configuration } from './config/configuration';
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { typeOrmConfig } from './config/typeorm.config';
 import { TasksModule } from './tasks/tasks.module';
 import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from '@nestjs/config';
 
 @Module({
-  imports: [TasksModule, TypeOrmModule.forRoot(typeOrmConfig), AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      load: [configuration],
+      validationSchema,
+    }),
+    TypeOrmModule.forRoot(typeOrmConfig),
+    TasksModule,
+    AuthModule,
+  ],
 })
 export class AppModule {}
